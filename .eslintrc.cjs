1538d3cf3b87567fccbf973440d640fcf262d0c7
Sajeel Khawaja <sajeel.waien@gmail.com>
2023-12-15 13:39:27 +0500

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,9 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    "no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
